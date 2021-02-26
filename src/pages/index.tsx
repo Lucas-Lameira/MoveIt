5c0d816bb90efa4eacac2e53c5f799a80f36526b
Lucas de souza lameira <lucasslameira@gmail.com>
2021-02-26 17:56:32 -0300

    Quinto dia de nlw trilha react Finalizadooo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import {GetServerSideProps} from 'next';
 import styles from '../styles/pages/Home.module.css';
 
 import ExperienceBar from '../components/ExperienceBar';
@@ -7,9 +8,24 @@ import CompletedChallenges from '../components/CompletedChallenges';
 import Countdown from '../components/Countdown';
 import ChallengeBox from '../components/ChallengeBox';
 import { CountdownProvider } from '../contexts/CountdownContext';
+import { ChallengesProvider } from '../contexts/ChallengeContext';
 
-export default function Home() {
+
+interface HomeProps {
+  level:number ;
+  currentExperience: number; 
+  challengesCompleted: number;
+}
+
+export default function Home(props: HomeProps) {
+  console.log(props)
   return (
+    <ChallengesProvider
+      level={props.level}
+      currentExperience={props.currentExperience}
+      challengesCompleted={props.challengesCompleted}
+
+    >
       <div className={styles.container}>       
         <Head>
           <title>Moveit</title>
@@ -29,6 +45,19 @@ export default function Home() {
             </div>
           </section>
         </CountdownProvider>
-    </div> 
+      </div> 
+    </ChallengesProvider>
   )
+}
+
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const {level, currentExperience, challengesCompleted} = ctx.req.cookies;
+  return {
+    props: {
+      level: Number(level),
+      currentExperience: Number(currentExperience),
+      challengesCompleted:  Number(challengesCompleted)
+    }
+  }
 }
